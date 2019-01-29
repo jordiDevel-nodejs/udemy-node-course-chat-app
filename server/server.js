24e482821bc496d76434c4030438e96f3457fd2a
jmedina <skeptofeina@gmail.com>
2019-01-29 19:54:03 +0100

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
@@ -28,6 +28,10 @@ io.on('connection', (socket) => {
         io.emit('newMessage', generateMessage(message.from, message.text));
 
         callback('This is from the server');
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,8 +1,8 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
-describe('', () => {
+describe('generateMessage', () => {
     it('should generate correct message object', () => {
         const from = 'Dembele';
         const text = 'Coutinho patata';
@@ -13,5 +13,21 @@ describe('', () => {
         expect(msg.text).toBe(text);
         expect(typeof msg.createdAt).toBe('number');
         expect(msg).toMatchObject({ from, text });
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location message object', () => {
+        const from = 'Dembele';
+        const latitude = 1;
+        const longitude = 1;
+        const url = 'https://www.google.com/maps?q=1,1';
+
+        const msg = generateLocationMessage(from, latitude, longitude);
+
+        expect(msg.from).toBe(from);
+        expect(msg.url).toBe(url);
+        expect(typeof msg.createdAt).toBe('number');
+        expect(msg).toMatchObject({ from, url });
     });
 });
